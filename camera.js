50d5b7ea91dce7fbd75b9ba47bedc722425cbaba
iMoxii <92295177+iMoxii@users.noreply.github.com>
2021-10-11 10:44:19 +0300

    Add files via upload

---
+++ camera.js
@@ -0,0 +1,28 @@
+var video= document.getElementById("video");
+
+//..........................................
+function on_cam_succes(stream){
+  video.srcObject=stream;
+}
+
+//..........................................
+function on_cam_error(err){
+  alert("error"+err.message);
+}
+
+//..........................................
+var constraints={audio: false, video:true};
+
+navigator.mediaDevices.getUserMedia(constraints)
+.then (on_cam_succes)
+.catch (on_cam_error);
+
+//..........................................
+function capteaza()
+{
+  var c=document.getElementById("canvas");
+  c.width=video.width;
+  c.height=video.width;
+  var ctx=c.getContext("2d");
+  ctx.drawImage(video, 0,0,640,480);
+}
